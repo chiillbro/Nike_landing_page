d39e7876b1c238fe93aa8012cbe4fa70616d12bc
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-03-06 18:53:02 +0530

    added files

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffers,
+  Subscribe,
+};
